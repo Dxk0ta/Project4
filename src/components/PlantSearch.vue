5896ab49f7bb57f452f9e7c16d9486a6b54bf3dc
dxk0ta <dxkotacamps@gmail.com>
2023-05-21 21:02:46 -0700

    Got API connected to application and got basic data (common name of plants) rendering on screen of search plants page

--- src/components/PlantSearch.vue
+++ src/components/PlantSearch.vue
@@ -1,7 +1,40 @@
 <template>
   <div>
     <h1>Search Plants Page</h1>
+
+    <div v-for="item in namesAndPictures" :key="item.id">
+      <h3>{{ item.common_name }}</h3>
+      <!-- <img :src="item.picture" alt="User picture" /> -->
+    </div>
   </div>
 </template>
 
+<script>
+export default {
+  data() {
+    return {
+      namesAndPictures: []
+    };
+  },
+  mounted() {
+    this.fetchData();
+  },
+  methods: {
+    async fetchData() {
+      try {
+        // const apiKey = /
+        const response = await fetch(`    https://perenual.com/api/species-list?page=1&key=${process.env.API_KEY}`);
+        const data = await response.json();
+        this.namesAndPictures = data.data;
+        console.log('names', this.namesAndPictures)
+      } catch (error) {
+        console.error(error);
+      }
+    }
+  }
+};
+</script>
 
+<style scoped>
+/* Add any custom styles for your component */
+</style>
--- vue.config.js
+++ vue.config.js
@@ -1,4 +1,13 @@
-const { defineConfig } = require('@vue/cli-service')
-module.exports = defineConfig({
-  transpileDependencies: true
-})
+const webpack = require('webpack');
+module.exports = {
+  transpileDependencies: true,
+  configureWebpack: {
+    plugins: [
+      new webpack.DefinePlugin({
+        'process.env': {
+          API_KEY: JSON.stringify(process.env.API_KEY)
+        }
+      })
+    ]
+  }
+};
